8a1b48398b9bbea3ec553331d6979ac8d5ede5bc
chang-chih-yao <m900054@gmail.com>
2019-03-09 14:34:02 +0800

    Add files via upload

---
+++ Client/Assets/CamStream/Scripts/VideoPanel.cs
@@ -0,0 +1,30 @@
+﻿//  
+// Copyright (c) 2017 Vulcan, Inc. All rights reserved.  
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+//
+
+using UnityEngine;
+using UnityEngine.UI;
+using OpenCVForUnity.CoreModule;
+using OpenCVForUnity.ImgcodecsModule;
+
+public class VideoPanel : MonoBehaviour
+{
+    public RawImage rawImage;
+    public Text debug_1;
+    public Text debug_2;
+
+    public void SetResolution(int width, int height)
+    {
+        var texture = new Texture2D(width, height, TextureFormat.BGRA32, false);
+        rawImage.texture = texture;
+    }
+
+    public void SetBytes(byte[] image)
+    {
+        
+        var texture = rawImage.texture as Texture2D;
+        texture.LoadRawTextureData(image); //TODO: Should be able to do this: texture.LoadRawTextureData(pointerToImage, 1280 * 720 * 4);
+        texture.Apply();
+    }
+}
