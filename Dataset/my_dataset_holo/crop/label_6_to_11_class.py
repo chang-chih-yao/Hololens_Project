dc45084da4875d230ff6fe10e860f1325029e7d2
chang-chih-yao_lab <m900054@gmail.com>
2019-05-18 22:02:55 +0800

    Update

--- Dataset/my_dataset_holo/crop/label_6_to_11_class.py
+++ Dataset/my_dataset_holo/crop/label_6_to_11_class.py
@@ -2,11 +2,18 @@ import cv2
 import os
 import numpy as np
 
-file_root = ['2/', '3/', '4/', '5/', '6/']
+'''
+generate "for_11_class.txt"
+'''
+
+#file_arr = ['2/', '3/', '4/', '5/', '6/']
+file_arr = ['2/', '3/']
 
 def main():
-    for i in range(len(file_root)):
-        print(len(os.listdir(file_root[i])))
+    for i in range(len(file_arr)):
+        print(len(os.listdir(file_arr[i])))
+        for dirPath, dirNames, fileNames in os.walk(file_arr[i]):
+            print(dirPath, fileNames)
 
 if __name__ == "__main__":
     main()
